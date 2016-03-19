fbdd2f1f414922ffdd472f76c975632ffd52fa04
MarisolenlaWeb <marcarrimor@gmail.com>
2016-03-19 11:41:54 -0600

    Nos salió el pryecto a último momento

--- jetpack/main/views.py
+++ jetpack/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
+
+class Main(View):
+    def get(self,request):
+        return render(request,'main/main.html')
